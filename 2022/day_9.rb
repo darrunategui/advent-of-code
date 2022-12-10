4ce5bc254ccec23e784389b32f1b6665d7f210a8
David Arrunategui <darrunategui@wealthsimple.com>
2022-12-09 22:22:16 -0500

    day 9 part 2

--- 2022/day_9.rb
+++ 2022/day_9.rb
@@ -1,3 +1,4 @@
+require 'set'
 require '../input_reader'
 input = InputReader.read.split("\n").map { |l| { direction: l.split(' ')[0], steps: l.split(' ')[1].to_i } }
 
@@ -14,31 +15,55 @@ class Coordinate
     Coordinate.new(x + MOVES[direction][0], y + MOVES[direction][1])
   end
 
+  def eql?(other)
+    self.x == other.x && self.y == other.y
+  end
+
+  def hash
+    [x, y].hash
+  end
+
   def to_s
     "[#{x}, #{y}]" 
   end
 end
 
 class Rope
-  attr_reader :head, :tail, :tail_history
-  def initialize
-    @head = Coordinate.new(0, 0)
-    @tail = Coordinate.new(0, 0)
-    @tail_history = []
+  attr_reader :tail_history
+  def initialize(knots)
+    @knots = (1..knots).map { |_| Coordinate.new(0, 0) }
+    @tail_history = Set.new
+  end
+
+  def head
+    @knots.first
+  end
+
+  def head=(coord)
+    @knots[0] = coord
+  end
+
+  def tail
+    @knots.last
   end
 
   def step(direction)
-    @head = @head.move(direction)
-    if knots_too_far?
-      @tail_history << @tail
-      @tail = slack_knot(@head, @tail)
+    self.head = self.head.move(direction)
+
+    for i in 0...@knots.length do
+      if i+1 == @knots.length
+        @tail_history << tail
+        break
+      elsif knots_too_far?(@knots[i], @knots[i+1])
+        @knots[i+1] = slack_knot(@knots[i], @knots[i+1])
+      end
     end
   end
 
   private
 
-  def knots_too_far?
-    (@head.x - @tail.x).abs > 1 || (@head.y - @tail.y).abs > 1
+  def knots_too_far?(ahead, behind)
+    (ahead.x - behind.x).abs > 1 || (ahead.y - behind.y).abs > 1
   end
 
   def slack_knot(ahead, behind)
@@ -62,13 +87,20 @@ class Rope
   end
 end
 
-rope = Rope.new
-#part 1
-input.each do |entry|
-  for step in 1..entry[:steps] do
-    rope.step(entry[:direction])
+def walk(input, rope)
+  input.each do |entry|
+    for step in 1..entry[:steps] do
+      rope.step(entry[:direction])
+    end
   end
 end
 
+#part 1
+rope = Rope.new(2)
+walk(input, rope)
 puts [*rope.tail_history, rope.tail].uniq { |c| [c.x, c.y] }.count
 # 6563
+
+rope = Rope.new(10)
+walk(input, rope)
+puts [*rope.tail_history, rope.tail].uniq { |c| [c.x, c.y] }.count
