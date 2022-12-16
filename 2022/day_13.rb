4ba10b95647aea9fff28ef85d917e05e1bc7ff68
David Arrunategui <darrunategui@wealthsimple.com>
2022-12-15 22:40:48 -0500

    day 13 part 2

--- 2022/day_13.rb
+++ 2022/day_13.rb
@@ -1,35 +1,34 @@
 require '../input_reader'
+
+def compare(left, right)
+  return 0 if left == right
+  return left <=> right if left.is_a?(Numeric) && right.is_a?(Numeric)
+  return compare([left], right) if left.is_a?(Numeric)
+  return compare(left, [right]) if right.is_a?(Numeric)
+  return -1 if left.length == 0
+  return 1 if right.length == 0
+
+  comparison = compare(left[0], right[0])
+  return comparison == 0 ? compare(left[1..], right[1..]) : comparison
+end
+
+# part 1
 input = InputReader.read.split("\n\n").map { |pair| 
   {
     left: eval(pair.split("\n")[0]),
     right: eval(pair.split("\n")[1]),
   }
 }
+puts input.each.with_index(1).filter_map { |pair, index|
+  index if compare(pair[:left], pair[:right]) == -1
+}.sum
 
-def compare(left, right)
-  #puts "comparing #{left} with  #{right}"
-  if left.is_a?(Numeric) && right.is_a?(Numeric)
-    return true if left < right
-    return false if left > right
-    return nil if left == right
-  end
+# part 2
+input = InputReader.read.split("\n").
+  filter { |l| !l.strip.empty? }.
+  push("[[2]]", "[[6]]").
+  map { |l| eval(l) }
 
-  left = [left] if left.is_a?(Numeric)
-  right = [right] if right.is_a?(Numeric)
-
-  for i in 0...[left.length, right.length].max
-    return true if i == left.length
-    return false if i == right.length 
-
-    comparison = compare(left[i], right[i])
-    #puts " = #{comparison}"
-    return comparison if !comparison.nil?
-  end
-  nil
-end
-
-# part 1
-in_order = input.each.with_index(1).filter_map { |pair, index|
-  index if compare(pair[:left], pair[:right]) == true
-}
-puts in_order.sum
+puts input.sort { |l, r| compare(l, r) }.filter_map.with_index(1) { |v, i|
+  i if v == [[2]] || v == [[6]]
+}.reduce(:*)
