00aebfc2afa39f8fea16e5fd6c0b8aadd5cd2463
David Arrunategui <darruna@uwo.ca>
2023-12-03 22:48:53 -0500

    day 3 complete

--- 2023/day_3.rb
+++ 2023/day_3.rb
@@ -3,38 +3,65 @@ require '../coordinate'
 input = InputReader.read
 
 class Schematic
-  attr_accessor :digit_coords, :symbol_coords
+  attr_accessor :digit_coords, :symbol_coords, :part_num_coords, :gear_ratio_num_coords
   def initialize(input)
-    @digit_coords = []
+    @digit_coords = Set.new
     @symbol_coords = Set.new
 
-    grid = input.split("\n").map(&:chars)
-    @grid = grid
-    for x in (0...grid.length)
-      digit_coords = []
-      for y in (0...grid[x].length)
+    @grid = input.split("\n").map(&:chars)
+    for x in (0...@grid.length)
+      working_range = []
+      for y in (0...@grid[x].length)
         coord = Coordinate.new(x,y)
-        if grid[x][y].match?(/\d/)
-          if digit_coords.empty? # found first digit
-            digit_coords << coord
-          elsif y+1 == grid[x].length || !grid[x][y+1].match?(/\d/) #found last digit
-            digit_coords << coord
-            digit_coords << digit_coords
-            digit_coords = []
-          else # middle of digit
-            next
+        if @grid[x][y].match?(/\d/)
+          if working_range.empty? # found first digit
+            working_range << coord
           end
-        elsif grid[x][y].match?(/\./)
+          if y+1 == @grid[x].length || !@grid[x][y+1].match?(/\d/) #found last digit
+            working_range << coord
+            digit_coords << working_range
+            working_range = []
+          end
+        elsif @grid[x][y].match?(/\./)
           next
         else
           symbol_coords << coord
         end
       end
     end
+
+    @part_num_coords = Set.new
+    @gear_ratio_num_coords = Set.new
+    symbol_coords.each { |symbol_coord|
+      is_gear = @grid[symbol_coord.x][symbol_coord.y] == "*"
+      found_part_nums = Set.new
+      [[-1,-1], [-1,0], [-1, 1], [0, 1], [1,1], [1,0], [1,-1], [0,-1]].map { |m| 
+        to_check = Coordinate.new(symbol_coord.x + m[0], symbol_coord.y + m[1])
+
+        digit_coords.each { |range|
+          if range.first.x == to_check.x && range.first.y <= to_check.y && to_check.y <= range.last.y
+            part_num_coords << range
+            found_part_nums << range
+          end
+        }
+      }
+
+      if is_gear && found_part_nums.length == 2
+        gear_ratio_num_coords << found_part_nums.to_a[0]
+        gear_ratio_num_coords << found_part_nums.to_a[1]
+      end
+    }
   end
 
-  def digit_nums
-    digit_coords.map { |range|
+  def part_numbers
+    part_num_coords.map { |range|
+      x = range.first.x
+      (range.first.y..range.last.y).map { |y| @grid[x][y] }.join.to_i
+    }
+  end
+
+  def gear_ratios
+    gear_ratio_num_coords.map { |range|
       x = range.first.x
       (range.first.y..range.last.y).map { |y| @grid[x][y] }.join.to_i
     }
@@ -42,14 +69,6 @@ class Schematic
 end
 
 schematic = Schematic.new(input)
-p schematic.digit_coords
+p schematic.part_numbers.sum
 
-real_part_nums = Set.new
-schematic.symbol_coords.each { |symbol_coord|
-  [[-1,-1], [-1,0], [-1, 1], [0, 1], [1,1], [1,0], [1,-1], [0,-1]].map { |m| 
-    Coordinate.new(symbol_coord.x + m[0], symbol_coord.y + m[1])
-  }
-  schematic.digit_coords { |range|
-    # :( unfinished
-  }
-}
+p schematic.gear_ratios.each_slice(2).map { |g| g.reduce(1, :*) }.sum
