e064fdbb27ec9d2a852a8f8758de3f87e6f356ae
David Arrunategui <darrunategui@wealthsimple.com>
2023-12-05 22:20:25 -0500

    optimized part 1

--- 2023/day_5.rb
+++ 2023/day_5.rb
@@ -1,20 +1,15 @@
 class AlmanacMap
-  attr_reader :value
   def initialize(legend)
     @maps = legend.each_slice(3).map { |dest, source, range|
-      min = source
-      max = source + range - 1
-      
-    }
-
-    @value = legend.each_slice(3).reduce({}) { |acc, v|
-      (0...v[2]).map { |i| acc[v[1]+i] = v[0]+i }
-      acc
+      { dest:, source_min: source, source_max: source + range - 1 }
     }
   end
 
   def get(source_num)
-    @value.fetch(source_num, source_num)
+    map = @maps.find { |m| m[:source_min] <= source_num && source_num <= m[:source_max] }
+    return source_num if map.nil?
+
+    map[:dest] + source_num - map[:source_min]
   end
 end
 
@@ -24,25 +19,21 @@ class AlmanacReader
   end
 
   def location(seed)
-    @maps.reduce(seed) { |acc, map|
-      map.get(acc)
-    }
+    @maps.reduce(seed) { |acc, map| map.get(acc) }
   end
 end
 
 require '../input_reader'
-seeds, *maps = InputReader.read.split(":").drop(1)
-  .map { |l| l.scan(/\d+/).map { |d| d.to_i } }
-#seed_to_soil,
-#soil_to_fertilizer,
-#fertilizer_to_water,
-#water_to_light,
-#light_to_temperature,
-#temperature_to_humidity,
-#humidity_to_location = maps.map { |m| AlmanacMap.new(m) }
+seeds, *maps = InputReader.read.split(':').drop(1).map { |l| l.scan(/\d+/).map(&:to_i) }
+maps = maps.map { |m| AlmanacMap.new(m) }.to_a
 
-reader = AlmanacReader.new(maps.map { |m| AlmanacMap.new(m) }.to_a)
+reader = AlmanacReader.new(maps)
 
+# part 1
 p seeds.map { |s| reader.location(s) }.min
 
-
+# part 2
+p seeds.each_slice(2)
+  .map { |seed, range| seed.upto(seed+range-1).to_a }
+  .flatten
+  .map { |s| reader.location(s) }.min
